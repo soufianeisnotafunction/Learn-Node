9abc6802924b3ddb0f825fc8c1ad2da05100b88d
Soufiane  Boutallaka <s.boutallaka@gmail.com>
2018-02-11 17:47:59 +0100

    first pug file

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  // res.send("Hey! It is working!");
+  // res.json(req.query);
+  const { name } = req.query;
+  res.render("hello", { name });
 });
 
+router.get("/reverse/:name", (req, res) => {
+  const reversedName = [...req.params.name].reverse().join("");
+  res.send(reversedName);
+});
 module.exports = router;
